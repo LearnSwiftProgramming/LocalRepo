4cfb67e1451ef4c31d5116e05f943d92e3ad4ea7
LearnSwiftProgramming <beadeveloper@gmail.com>
2024-09-21 23:16:44 +0530

    Add new project

---
+++ Gitcmd_Demo2/Gitcmd_Demo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gitcmd_Demo2
+//
+//  Created by Rahul Ajmera on 21/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        printContent("New demo")
+    }
+
+
+}
+
