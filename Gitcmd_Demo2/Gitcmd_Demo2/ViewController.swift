9bc1e35f2aed8b5c71e6eac92b76e3fe30bab4ec
LearnSwiftProgramming <beadeveloper@gmail.com>
2024-09-22 00:01:19 +0530

    added data

--- Gitcmd_Demo2/Gitcmd_Demo2/ViewController.swift
+++ Gitcmd_Demo2/Gitcmd_Demo2/ViewController.swift
@@ -7,7 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
